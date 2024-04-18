b4031900e5d60d1272ed8ef51469814f5acdf1f5
Pepe <a01620480@itesm.mx>
2024-04-18 16:43:24 -0600

    add controller

---
+++ scripts/controler.py
@@ -0,0 +1,20 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+def talker():
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        vel_msg = Twist()
+        vel_msg.linear.x = 0.5 # Change this value to what you want
+        vel_msg.angular.z = 0.1 # Change this value to what you want
+        rospy.loginfo(vel_msg)
+        pub.publish(vel_msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
